ae99a669d6e68557e87cd80d5690242a8709167c
mihir867 <c-mihir.thakur@devrev.ai>
2025-05-01 14:43:51 +0530

    lfgg build fix

--- breachx/lib/auth.ts
+++ breachx/lib/auth.ts
@@ -10,7 +10,7 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
       profile(profile) {
         return {
-          id: profile.id.toString(),d
+          id: profile.id.toString(),
           name: profile.name || profile.login,
           email: profile.email,
           image: profile.avatar_url,
